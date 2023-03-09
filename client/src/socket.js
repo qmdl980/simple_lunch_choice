0dc3e99c5f813ff8da7fabcca578e32a067313f6
HYUKJIN OH <qmdl98056@gmail.com>
2023-03-10 02:11:30 +0900

    [feat]: 개발, 배포환경 분리

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
+import config from "./config.json"
 
-export const socket = io("http://localhost:8080", {
+export const socket = io(`${config.Server}:${config.Port}`, {
   cors: {
     origin: "*",
   },
