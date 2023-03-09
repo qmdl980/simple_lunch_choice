0390c24df2a916c5eb5c1963209915308fd9e277
qmdl980 <qmdl98056@gmail.com>
2023-03-09 17:08:50 +0900

    [fix]: dummy data 삭제

--- server/index.js
+++ server/index.js
@@ -13,19 +13,7 @@ const server = io(httpServer, {
 });
 
 const sockets = [];
-let lunchList = [
-  { id: 1, name: "국밥", people: 1 },
-  { id: 2, name: "국밥", people: 2 },
-  { id: 3, name: "국밥", people: 3 },
-  { id: 4, name: "국밥", people: 4 },
-  { id: 5, name: "국밥", people: 5 },
-  { id: 6, name: "국밥", people: 6 },
-  { id: 7, name: "국밥", people: 7 },
-  { id: 8, name: "국밥", people: 8 },
-  { id: 9, name: "국밥", people: 9 },
-  { id: 10, name: "국밥", people: 10 },
-  { id: 11, name: "국밥", people: 11 },
-];
+let lunchList = [];
 
 server.on("connect", (socket) => {
   console.log("client connected");
